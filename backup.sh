7fd371a0f0a120ec496034cb77faa1c70f1e81ed
bigstones <71265839+bigstones@users.noreply.github.com>
2020-12-09 15:09:39 +0900

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
 tar cvpzf /backup.tgz.gz --exclude=/proc --exclude=/lost+found --exclude=/media --exclude=/backup.tgz --exclude=/mnt --exclude=/sys /
 
-echo 'tar xvpfz /backup.tgz -C / 여기가 복원'
+echo 'tar xvpfz /backup.tgz -C / 이 명령이 복원'
