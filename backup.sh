92a79965f0371a9b3a08c3f21a691e0d55d6e854
bigstones <71265839+bigstones@users.noreply.github.com>
2020-12-10 15:54:05 +0900

    오타 수정

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
-tar cvpzf /backup.tgz.gz --exclude=/proc --exclude=/lost+found --exclude=/media --exclude=/backup.tgz --exclude=/mnt --exclude=/sys /
+tar cvpzf /backup.tgz --exclude=/proc --exclude=/lost+found --exclude=/media --exclude=/backup.tgz --exclude=/mnt --exclude=/sys /
 
 echo 'tar xvpfz /backup.tgz -C / 이 명령이 복원'
